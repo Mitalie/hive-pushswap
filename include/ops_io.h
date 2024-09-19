0023ce97520a5e0149a6d3a9e2bad8902d25f6d8
Anssi Mäkinen <amakinen@student.hive.fi>
2024-09-19 13:44:29 +0300

    ops_io: Add comments

--- include/ops_io.h
+++ include/ops_io.h
@@ -6,7 +6,7 @@
 /*   By: amakinen <amakinen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 15:22:04 by amakinen          #+#    #+#             */
-/*   Updated: 2024/09/09 16:29:55 by amakinen         ###   ########.fr       */
+/*   Updated: 2024/09/19 13:44:11 by amakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,17 @@
 # include "status.h"
 # include "ops.h"
 
+/*
+	Buffer size must be at least the longest valid line including the newline
+	character, but a larger buffer reduces the number of read system calls.
+*/
 # define READ_OPS_BUF_SIZE 256
 
+/*
+	A struct to store the state of reading ops from a particular file
+	descriptor. The fields should be considered private, but are defined here to
+	allow stack allocation of the struct.
+*/
 typedef struct s_read_ops_state
 {
 	char	buf[READ_OPS_BUF_SIZE];
@@ -28,9 +37,24 @@ typedef struct s_read_ops_state
 	size_t	len;
 }	t_read_ops_state;
 
+/*
+	Initialize `t_read_ops_state` for reading ops from a particular file
+	descriptor.
+*/
 void		read_ops_init(t_read_ops_state *s, int fd);
+
+/*
+	Read next op from the file. If an op is successfully read and parsed, the op
+	is written to `op` and false is written to `end`. If end of file has been
+	reached and no more ops are available, true is written to `end`. If a read
+	error occurs or invalid input is detected, an error code is returned
+	instead.
+*/
 t_ps_status	read_ops_next(t_read_ops_state *s, bool *end, t_ps_op *op);
 
+/*
+	Write the string representation of an op to the given file descriptor.
+*/
 t_ps_status	write_op(int fd, t_ps_op op);
 
 #endif
